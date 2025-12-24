8a80015f9b872e7cfbb561317308391bffdf4625
takatsugu-nakayama <takatsugu.nakayama@dena.com>
2025-12-24 17:00:58 +0900

    RequestとResponse内にタブを移動

--- src/main/java/core/packetproxy/gui/GUIPacket.java
+++ src/main/java/core/packetproxy/gui/GUIPacket.java
@@ -19,9 +19,6 @@ import static packetproxy.util.Logging.errWithStackTrace;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import packetproxy.model.Packet;
 import packetproxy.model.Packets;
 
@@ -29,28 +26,9 @@ public class GUIPacket {
 
 	private static GUIPacket instance;
 	private JFrame owner;
-	private GUIDataAll all_panel;
-	private JTabbedPane packet_pane;
-	private GUIData received_panel;
-	private GUIData decoded_panel;
-	private GUIData modified_panel;
-	private GUIData sent_panel;
 	private GUIRequestResponsePanel request_response_panel;
 	private Packet showing_packet;
 	private Packet showing_response_packet;
-
-	// public static void main(String args[])
-	// {
-	// try {
-	// GUIPacket gui = new GUIPacket();
-	// String s = "ABgNBHJfb2sAAAJhbANtc2cAB4NoAmEMYQANCg0KeyJlbXB0eSI6N30=";
-	// byte[] data = Base64.getDecoder().decode(s.getBytes());
-	// byte[] result = gui.prettyFormatJSONInRawData(data, "hoge");
-	// Logging.log(new String(result));
-	// } catch (Exception e) {
-	// errWithStackTrace(e);
-	// }
-	// }
 
 	public static GUIPacket getInstance() throws Exception {
 		if (instance == null) {
@@ -67,81 +45,21 @@ public class GUIPacket {
 	}
 
 	public JComponent createPanel() throws Exception {
-		received_panel = new GUIData(this.owner);
-		decoded_panel = new GUIData(this.owner);
-		modified_panel = new GUIData(this.owner);
-		sent_panel = new GUIData(this.owner);
-		all_panel = new GUIDataAll();
 		request_response_panel = new GUIRequestResponsePanel(this.owner);
-
-		packet_pane = new JTabbedPane();
-		packet_pane.addTab("Request / Response", request_response_panel.createPanel());
-		packet_pane.addTab("Received Packet", received_panel.createPanel());
-		packet_pane.addTab("Decoded", decoded_panel.createPanel());
-		packet_pane.addTab("Modified", modified_panel.createPanel());
-		packet_pane.addTab("Encoded (Sent Packet)", sent_panel.createPanel());
-		packet_pane.addTab("All", all_panel.createPanel());
-		packet_pane.addChangeListener(new ChangeListener() {
-
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				try {
-
-					update();
-				} catch (Exception e1) {
-
-					errWithStackTrace(e1);
-				}
-			}
-		});
-		packet_pane.setSelectedIndex(0); /* Request / Response */
-		return packet_pane;
+		return request_response_panel.createPanel();
 	}
 
 	public byte[] getData() {
-		switch (packet_pane.getSelectedIndex()) {
-			case 0 :
-				return request_response_panel.getRequestData();
-			case 1 :
-				return received_panel.getData();
-			case 2 :
-				return decoded_panel.getData();
-			case 3 :
-				return modified_panel.getData();
-			case 4 :
-				return sent_panel.getData();
-			default :
-				return modified_panel.getData();
-		}
+		return request_response_panel.getRequestData();
 	}
 
 	public void update() {
-		if (showing_packet == null) {
+		if (showing_packet == null && showing_response_packet == null) {
 
 			return;
 		}
-		switch (packet_pane.getSelectedIndex()) {
-			case 0 :
-				request_response_panel.setRequestPacket(showing_packet);
-				request_response_panel.setResponsePacket(showing_response_packet);
-				break;
-			case 1 :
-				received_panel.setData(showing_packet.getReceivedData());
-				break;
-			case 2 :
-				decoded_panel.setData(showing_packet.getDecodedData());
-				break;
-			case 3 :
-				modified_panel.setData(showing_packet.getModifiedData());
-				break;
-			case 4 :
-				sent_panel.setData(showing_packet.getSentData());
-				break;
-			case 5 :
-				all_panel.setPacket(showing_packet);
-				break;
-			default :
-		}
+		request_response_panel.setRequestPacket(showing_packet);
+		request_response_panel.setResponsePacket(showing_response_packet);
 	}
 
 	public void setPacket(Packet packet) {
--- src/main/java/core/packetproxy/gui/GUIRequestResponsePanel.java
+++ src/main/java/core/packetproxy/gui/GUIRequestResponsePanel.java
@@ -19,113 +19,366 @@ import static packetproxy.util.Logging.errWithStackTrace;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import packetproxy.common.I18nString;
 import packetproxy.model.Packet;
 
 /**
  * リクエストとレスポンスを左右に並べて表示するパネル
+ * 各パネルにReceived Packet, Decoded, Modified, Encoded, Allのタブを持つ
  */
 public class GUIRequestResponsePanel {
 
 	private JPanel main_panel;
-	private TabSet request_tabs;
-	private TabSet response_tabs;
-	private JPanel request_panel;
-	private JPanel response_panel;
 	private JSplitPane split_pane;
 
-	public GUIRequestResponsePanel(@SuppressWarnings("unused") javax.swing.JFrame owner) {
-		// ownerは将来の拡張用に受け取るが、現在は使用しない
+	// Request側
+	private JPanel request_panel;
+	private JTabbedPane request_tabs;
+	private TabSet request_decoded_tabs;
+	private GUIData request_received_panel;
+	private GUIData request_modified_panel;
+	private GUIData request_sent_panel;
+	private JComponent request_all_panel;
+	private RawTextPane request_all_received;
+	private RawTextPane request_all_decoded;
+	private RawTextPane request_all_modified;
+	private RawTextPane request_all_sent;
+
+	// Response側
+	private JPanel response_panel;
+	private JTabbedPane response_tabs;
+	private TabSet response_decoded_tabs;
+	private GUIData response_received_panel;
+	private GUIData response_modified_panel;
+	private GUIData response_sent_panel;
+	private JComponent response_all_panel;
+	private RawTextPane response_all_received;
+	private RawTextPane response_all_decoded;
+	private RawTextPane response_all_modified;
+	private RawTextPane response_all_sent;
+
+	// 現在表示中のパケット
+	private Packet showing_request_packet;
+	private Packet showing_response_packet;
+
+	private javax.swing.JFrame owner;
+
+	public GUIRequestResponsePanel(javax.swing.JFrame owner) {
+		this.owner = owner;
 	}
 
 	public JComponent createPanel() throws Exception {
 		main_panel = new JPanel();
 		main_panel.setLayout(new BorderLayout());
 
-		// リクエストパネル
-		request_panel = new JPanel();
-		request_panel.setLayout(new BoxLayout(request_panel, BoxLayout.Y_AXIS));
-		request_panel.setBorder(BorderFactory.createTitledBorder(
+		// リクエストパネルの作成
+		request_panel = createRequestPanel();
+
+		// レスポンスパネルの作成
+		response_panel = createResponsePanel();
+
+		// 左右に分割
+		split_pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, request_panel, response_panel);
+		split_pane.setResizeWeight(0.5);
+		split_pane.setOneTouchExpandable(true);
+		split_pane.setContinuousLayout(true);
+		split_pane.setDividerSize(8);
+
+		main_panel.add(split_pane, BorderLayout.CENTER);
+		return main_panel;
+	}
+
+	private JPanel createRequestPanel() throws Exception {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createTitledBorder(
 				BorderFactory.createLineBorder(new Color(0x33, 0x99, 0xff), 2),
 				"Request",
 				TitledBorder.LEFT,
 				TitledBorder.TOP,
 				null,
 				new Color(0x33, 0x99, 0xff)));
-		request_tabs = new TabSet(true, false);
-		request_panel.add(request_tabs.getTabPanel());
+		// JSplitPaneでリサイズできるように最小サイズを設定
+		panel.setMinimumSize(new Dimension(100, 100));
 
-		// レスポンスパネル
-		response_panel = new JPanel();
-		response_panel.setLayout(new BoxLayout(response_panel, BoxLayout.Y_AXIS));
-		response_panel.setBorder(BorderFactory.createTitledBorder(
+		// タブの作成
+		request_tabs = new JTabbedPane();
+
+		// Decoded タブ（メインのタブ）
+		request_decoded_tabs = new TabSet(true, false);
+		request_tabs.addTab("Decoded", request_decoded_tabs.getTabPanel());
+
+		// Received Packet タブ
+		request_received_panel = new GUIData(owner);
+		request_tabs.addTab("Received Packet", request_received_panel.createPanel());
+
+		// Modified タブ
+		request_modified_panel = new GUIData(owner);
+		request_tabs.addTab("Modified", request_modified_panel.createPanel());
+
+		// Encoded (Sent Packet) タブ
+		request_sent_panel = new GUIData(owner);
+		request_tabs.addTab("Encoded (Sent Packet)", request_sent_panel.createPanel());
+
+		// All タブ
+		request_all_panel = createAllPanel(true);
+		request_tabs.addTab("All", request_all_panel);
+
+		// タブ変更リスナー
+		request_tabs.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				updateRequestPanel();
+			}
+		});
+
+		panel.add(request_tabs, BorderLayout.CENTER);
+		return panel;
+	}
+
+	private JPanel createResponsePanel() throws Exception {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createTitledBorder(
 				BorderFactory.createLineBorder(new Color(0x99, 0x33, 0x33), 2),
 				"Response",
 				TitledBorder.LEFT,
 				TitledBorder.TOP,
 				null,
 				new Color(0x99, 0x33, 0x33)));
-		response_tabs = new TabSet(true, false);
-		response_panel.add(response_tabs.getTabPanel());
+		// JSplitPaneでリサイズできるように最小サイズを設定
+		panel.setMinimumSize(new Dimension(100, 100));
 
-		// 左右に分割
-		split_pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, request_panel, response_panel);
-		split_pane.setResizeWeight(0.5);
-		split_pane.setOneTouchExpandable(true);
+		// タブの作成
+		response_tabs = new JTabbedPane();
 
-		main_panel.add(split_pane, BorderLayout.CENTER);
-		return main_panel;
+		// Decoded タブ（メインのタブ）
+		response_decoded_tabs = new TabSet(true, false);
+		response_tabs.addTab("Decoded", response_decoded_tabs.getTabPanel());
+
+		// Received Packet タブ
+		response_received_panel = new GUIData(owner);
+		response_tabs.addTab("Received Packet", response_received_panel.createPanel());
+
+		// Modified タブ
+		response_modified_panel = new GUIData(owner);
+		response_tabs.addTab("Modified", response_modified_panel.createPanel());
+
+		// Encoded (Sent Packet) タブ
+		response_sent_panel = new GUIData(owner);
+		response_tabs.addTab("Encoded (Sent Packet)", response_sent_panel.createPanel());
+
+		// All タブ
+		response_all_panel = createAllPanel(false);
+		response_tabs.addTab("All", response_all_panel);
+
+		// タブ変更リスナー
+		response_tabs.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				updateResponsePanel();
+			}
+		});
+
+		panel.add(response_tabs, BorderLayout.CENTER);
+		return panel;
+	}
+
+	private JComponent createAllPanel(boolean isRequest) throws Exception {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 4));
+
+		RawTextPane received = createTextPaneForAll(panel, I18nString.get("Received"));
+		RawTextPane decoded = createTextPaneForAll(panel, I18nString.get("Decoded"));
+		RawTextPane modified = createTextPaneForAll(panel, I18nString.get("Modified"));
+		RawTextPane sent = createTextPaneForAll(panel, I18nString.get("Encoded"));
+
+		if (isRequest) {
+			request_all_received = received;
+			request_all_decoded = decoded;
+			request_all_modified = modified;
+			request_all_sent = sent;
+		} else {
+			response_all_received = received;
+			response_all_decoded = decoded;
+			response_all_modified = modified;
+			response_all_sent = sent;
+		}
+
+		return panel;
+	}
+
+	private RawTextPane createTextPaneForAll(JPanel parentPanel, String labelName) throws Exception {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+		JLabel label = new JLabel(labelName);
+		label.setAlignmentX(0.5f);
+
+		RawTextPane text = new RawTextPane();
+		text.setEditable(false);
+		panel.add(label);
+		JScrollPane scroll = new JScrollPane(text);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		panel.add(scroll);
+		parentPanel.add(panel);
+		return text;
 	}
 
 	public void setRequestPacket(Packet packet) {
-		if (packet == null) {
-			request_tabs.setData(new byte[]{});
+		showing_request_packet = packet;
+		updateRequestPanel();
+	}
+
+	public void setResponsePacket(Packet packet) {
+		showing_response_packet = packet;
+		updateResponsePanel();
+	}
+
+	private void updateRequestPanel() {
+		if (showing_request_packet == null) {
+			clearRequestPanel();
 			return;
 		}
 		try {
-			byte[] data = packet.getDecodedData();
-			if (data == null || data.length == 0) {
-				data = packet.getModifiedData();
+			int selectedIndex = request_tabs.getSelectedIndex();
+			switch (selectedIndex) {
+				case 0: // Decoded
+					byte[] decodedData = showing_request_packet.getDecodedData();
+					if (decodedData == null || decodedData.length == 0) {
+						decodedData = showing_request_packet.getModifiedData();
+					}
+					if (decodedData == null) {
+						decodedData = new byte[]{};
+					}
+					request_decoded_tabs.setData(decodedData);
+					break;
+				case 1: // Received Packet
+					request_received_panel.setData(showing_request_packet.getReceivedData());
+					break;
+				case 2: // Modified
+					request_modified_panel.setData(showing_request_packet.getModifiedData());
+					break;
+				case 3: // Encoded (Sent Packet)
+					request_sent_panel.setData(showing_request_packet.getSentData());
+					break;
+				case 4: // All
+					request_all_received.setData(showing_request_packet.getReceivedData(), true);
+					request_all_received.setCaretPosition(0);
+					request_all_decoded.setData(showing_request_packet.getDecodedData(), true);
+					request_all_decoded.setCaretPosition(0);
+					request_all_modified.setData(showing_request_packet.getModifiedData(), true);
+					request_all_modified.setCaretPosition(0);
+					request_all_sent.setData(showing_request_packet.getSentData(), true);
+					request_all_sent.setCaretPosition(0);
+					break;
 			}
-			if (data == null) {
-				data = new byte[]{};
-			}
-			request_tabs.setData(data);
 		} catch (Exception e) {
 			errWithStackTrace(e);
 		}
 	}
 
-	public void setResponsePacket(Packet packet) {
-		if (packet == null) {
-			response_tabs.setData(new byte[]{});
+	private void updateResponsePanel() {
+		if (showing_response_packet == null) {
+			clearResponsePanel();
 			return;
 		}
 		try {
-			byte[] data = packet.getDecodedData();
-			if (data == null || data.length == 0) {
-				data = packet.getModifiedData();
+			int selectedIndex = response_tabs.getSelectedIndex();
+			switch (selectedIndex) {
+				case 0: // Decoded
+					byte[] decodedData = showing_response_packet.getDecodedData();
+					if (decodedData == null || decodedData.length == 0) {
+						decodedData = showing_response_packet.getModifiedData();
+					}
+					if (decodedData == null) {
+						decodedData = new byte[]{};
+					}
+					response_decoded_tabs.setData(decodedData);
+					break;
+				case 1: // Received Packet
+					response_received_panel.setData(showing_response_packet.getReceivedData());
+					break;
+				case 2: // Modified
+					response_modified_panel.setData(showing_response_packet.getModifiedData());
+					break;
+				case 3: // Encoded (Sent Packet)
+					response_sent_panel.setData(showing_response_packet.getSentData());
+					break;
+				case 4: // All
+					response_all_received.setData(showing_response_packet.getReceivedData(), true);
+					response_all_received.setCaretPosition(0);
+					response_all_decoded.setData(showing_response_packet.getDecodedData(), true);
+					response_all_decoded.setCaretPosition(0);
+					response_all_modified.setData(showing_response_packet.getModifiedData(), true);
+					response_all_modified.setCaretPosition(0);
+					response_all_sent.setData(showing_response_packet.getSentData(), true);
+					response_all_sent.setCaretPosition(0);
+					break;
 			}
-			if (data == null) {
-				data = new byte[]{};
-			}
-			response_tabs.setData(data);
+		} catch (Exception e) {
+			errWithStackTrace(e);
+		}
+	}
+
+	private void clearRequestPanel() {
+		try {
+			request_decoded_tabs.setData(new byte[]{});
+			request_received_panel.setData(new byte[]{});
+			request_modified_panel.setData(new byte[]{});
+			request_sent_panel.setData(new byte[]{});
+			request_all_received.setData(new byte[]{}, true);
+			request_all_decoded.setData(new byte[]{}, true);
+			request_all_modified.setData(new byte[]{}, true);
+			request_all_sent.setData(new byte[]{}, true);
+		} catch (Exception e) {
+			errWithStackTrace(e);
+		}
+	}
+
+	private void clearResponsePanel() {
+		try {
+			response_decoded_tabs.setData(new byte[]{});
+			response_received_panel.setData(new byte[]{});
+			response_modified_panel.setData(new byte[]{});
+			response_sent_panel.setData(new byte[]{});
+			response_all_received.setData(new byte[]{}, true);
+			response_all_decoded.setData(new byte[]{}, true);
+			response_all_modified.setData(new byte[]{}, true);
+			response_all_sent.setData(new byte[]{}, true);
 		} catch (Exception e) {
 			errWithStackTrace(e);
 		}
 	}
 
 	public byte[] getRequestData() {
-		return request_tabs.getData();
+		if (showing_request_packet == null) {
+			return new byte[]{};
+		}
+		// Decodedタブからデータを取得
+		return request_decoded_tabs.getData();
 	}
 
 	public byte[] getResponseData() {
-		return response_tabs.getData();
+		if (showing_response_packet == null) {
+			return new byte[]{};
+		}
+		// Decodedタブからデータを取得
+		return response_decoded_tabs.getData();
 	}
 }
-

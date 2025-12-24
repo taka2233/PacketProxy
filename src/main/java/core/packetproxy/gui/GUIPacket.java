3c0419c99034b90d4adcd0c56aba2be9b10da881
takatsugu-nakayama <takatsugu.nakayama@dena.com>
2025-12-24 14:15:14 +0900

    リクエストとレスポンスを同じ行に表示

--- src/main/java/core/packetproxy/gui/GUIPacket.java
+++ src/main/java/core/packetproxy/gui/GUIPacket.java
@@ -23,6 +23,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import packetproxy.model.Packet;
+import packetproxy.model.Packets;
 
 public class GUIPacket {
 
@@ -34,7 +35,9 @@ public class GUIPacket {
 	private GUIData decoded_panel;
 	private GUIData modified_panel;
 	private GUIData sent_panel;
+	private GUIRequestResponsePanel request_response_panel;
 	private Packet showing_packet;
+	private Packet showing_response_packet;
 
 	// public static void main(String args[])
 	// {
@@ -60,6 +63,7 @@ public class GUIPacket {
 	private GUIPacket() throws Exception {
 		this.owner = GUIHistory.getOwner();
 		this.showing_packet = null;
+		this.showing_response_packet = null;
 	}
 
 	public JComponent createPanel() throws Exception {
@@ -68,8 +72,10 @@ public class GUIPacket {
 		modified_panel = new GUIData(this.owner);
 		sent_panel = new GUIData(this.owner);
 		all_panel = new GUIDataAll();
+		request_response_panel = new GUIRequestResponsePanel(this.owner);
 
 		packet_pane = new JTabbedPane();
+		packet_pane.addTab("Request / Response", request_response_panel.createPanel());
 		packet_pane.addTab("Received Packet", received_panel.createPanel());
 		packet_pane.addTab("Decoded", decoded_panel.createPanel());
 		packet_pane.addTab("Modified", modified_panel.createPanel());
@@ -88,19 +94,21 @@ public class GUIPacket {
 				}
 			}
 		});
-		packet_pane.setSelectedIndex(1); /* decoded */
+		packet_pane.setSelectedIndex(0); /* Request / Response */
 		return packet_pane;
 	}
 
 	public byte[] getData() {
 		switch (packet_pane.getSelectedIndex()) {
 			case 0 :
-				return received_panel.getData();
+				return request_response_panel.getRequestData();
 			case 1 :
-				return decoded_panel.getData();
+				return received_panel.getData();
 			case 2 :
-				return modified_panel.getData();
+				return decoded_panel.getData();
 			case 3 :
+				return modified_panel.getData();
+			case 4 :
 				return sent_panel.getData();
 			default :
 				return modified_panel.getData();
@@ -114,18 +122,22 @@ public class GUIPacket {
 		}
 		switch (packet_pane.getSelectedIndex()) {
 			case 0 :
-				received_panel.setData(showing_packet.getReceivedData());
+				request_response_panel.setRequestPacket(showing_packet);
+				request_response_panel.setResponsePacket(showing_response_packet);
 				break;
 			case 1 :
-				decoded_panel.setData(showing_packet.getDecodedData());
+				received_panel.setData(showing_packet.getReceivedData());
 				break;
 			case 2 :
-				modified_panel.setData(showing_packet.getModifiedData());
+				decoded_panel.setData(showing_packet.getDecodedData());
 				break;
 			case 3 :
-				sent_panel.setData(showing_packet.getSentData());
+				modified_panel.setData(showing_packet.getModifiedData());
 				break;
 			case 4 :
+				sent_panel.setData(showing_packet.getSentData());
+				break;
+			case 5 :
 				all_panel.setPacket(showing_packet);
 				break;
 			default :
@@ -139,11 +151,34 @@ public class GUIPacket {
 		} else {
 
 			showing_packet = packet;
+			// マージされた行の場合、レスポンスパケットも取得
+			try {
+				GUIHistory history = GUIHistory.getInstance();
+				int responsePacketId = history.getResponsePacketIdForRequest(packet.getId());
+				if (responsePacketId != -1) {
+					showing_response_packet = Packets.getInstance().query(responsePacketId);
+				} else {
+					// リクエストパケットがレスポンスパケットの場合、またはマージされていない場合
+					if (packet.getDirection() == Packet.Direction.SERVER) {
+						showing_response_packet = packet;
+						showing_packet = null;
+					} else {
+						showing_response_packet = null;
+					}
+				}
+			} catch (Exception e) {
+				errWithStackTrace(e);
+				showing_response_packet = null;
+			}
 		}
 		update();
 	}
 
 	public Packet getPacket() {
 		return showing_packet;
+	}
+
+	public Packet getResponsePacket() {
+		return showing_response_packet;
 	}
 }
